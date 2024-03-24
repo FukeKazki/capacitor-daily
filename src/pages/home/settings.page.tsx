578121f874003935a1f1ebb475ac0a1b80b41ba1
FukeKazki <kazkichi0906@gmail.com>
2024-03-24 20:19:03 +0900

    add settings

--- src/pages/home/settings.page.tsx
+++ src/pages/home/settings.page.tsx
@@ -1,6 +1,19 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonContent, IonHeader, IonItem, IonList, IonListHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+import { Dialog } from '@capacitor/dialog';
+import { clearAllDaily } from "../../features/daily/repository";
+import { useSWRConfig } from "swr";
 
 export default function SettingsPage() {
+  const { mutate } = useSWRConfig()
+  const handleDelete = async () => {
+    const { value } = await Dialog.confirm({
+      message: 'データを削除しますか？',
+    });
+    if (value) {
+      await clearAllDaily()
+      await mutate('/daily')
+    }
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -9,7 +22,12 @@ export default function SettingsPage() {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen={true}>
-        <h1>Settings</h1>
+        <IonList>
+          <IonListHeader>アカウント</IonListHeader>
+          <IonItem>
+            <IonButton onClick={handleDelete}>データ削除</IonButton>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   )
